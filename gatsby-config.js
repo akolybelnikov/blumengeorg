6bbc2e6cb7d4183d0d68e9f36249b4967a7db143
akolybelnikov <a.kolybelnikov@gmail.com>
2018-08-26 13:14:20 +0200

    initial setup

---
+++ gatsby-config.js
@@ -0,0 +1,137 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+const config = require('./siteConfig')
+
+module.exports = {
+  siteMetadata: {
+    title: 'Blumenhaus Georg',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.siteTitle,
+        short_name: config.shortTitle,
+        description: config.siteDescription,
+        start_url: '/',
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'minimal-ui',
+        icon: 'src/images/logo-lg.png',
+        icons: [
+          {
+            src: `icons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-57x57.png`,
+            sizes: `57x57`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-60x60.png`,
+            sizes: `60x60`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-76x76.png`,
+            sizes: `76x76`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-114x114.png`,
+            sizes: `114x114`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-120x120.png`,
+            sizes: `120x120`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/mstile-150x150.png`,
+            sizes: `150x150`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-152x152.png`,
+            sizes: `152x152`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-180x180.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-touch-icon.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-icon-precomposed.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-256x256.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/iTunesArtwork@1x.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/iTunesArtwork@2x.png`,
+            sizes: `1024x1024`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/iTunesArtwork@3x.png`,
+            sizes: `1536x1536`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ siteConfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+    siteTitle: 'Blumenhaus Georg',
+    shortTitle: 'BlumenGeorg', // Used for App manifest e.g. Mobile Home Screen
+    siteTitleAlt: 'Blumenhaus Georg', // This allows an alternative site title for SEO schema.
+    publisher: 'Blumenhaus Georg', // Organization name used for SEO schema
+    siteDescription:
+      'Blumenladen Georg Koeln, Flower shop Georg in Cologne',
+    siteUrl: 'https://blumengeorg.de', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    author: 'Andrey Kolybelnikov', // Author for RSS author segment and SEO schema
+    authorUrl: 'https://github.com/akolybelnikov', // URL used for author and publisher schema, can be a social profile or other personal site
+    userTwitter: '@rockapi', // Change for Twitter Cards    
+    shareImageWidth: 900, // Change to the width of your default share image
+    shareImageHeight: 600, // Change to the height of your default share image
+    backgroundColor: '#663399', // Used for Offline Manifest
+    themeColor: '#121212', // Used for Offline Manifest
+    copyright: 'Copyright © 2018 Blumenhaus Georg', // Copyright string for the RSS feed
+  }
+  
