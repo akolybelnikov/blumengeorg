7c2a1c19ada65588a04a86b70f988e8464bf7748
akolybelnikov <a.kolybelnikov@gmail.com>
2018-09-02 14:43:28 +0200

    footer details

--- src/components/Address/Telephone.js
+++ src/components/Address/Telephone.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Phone from '../icons/Phone'
 import Responsive from 'react-responsive'
-import './Adress.scss'
+import './Address.scss'
 
 const Mobile = props => <Responsive {...props} maxWidth={767} orientation={'portrait'}/>
 const MobileLandscape = props => (
@@ -9,39 +9,10 @@ const MobileLandscape = props => (
 )
 const DefaultGeneral = props => <Responsive {...props} minWidth={768} />
 
-const Container = styled.div`
-  display: grid;
-  grid-template: auto / 1fr 3fr;
-`
-const TelephoneNumber = styled.div`
-  grid-column: 2;
-  align-self: center;
-  @media screen and (min-width: 1024px) {
-    font-size: 1.425em;
-  }
-  @media screen and (max-width: 414px) {
-    font-size: 0.6em;
-  }
-  @media screen and (max-width: 823px) and (orientation: landscape) {
-    font-size: .8em;
-  }
-  @media screen and (min-width: 768px) and (max-width: 1023px) and (orientation: portrait) {
-    font-size: 1.425em;
-  }
-  p {
-    margin: 2px;
-  }
-`
-const IconPhone = styled.div`
-  grid-column: 1;
-  align-self: center;
-  justify-self: center;
-`
-
 const Telephone = props => {
   return (
-    <Container>
-      <IconPhone>
+    <div id="telephone-container">
+      <div id="icon-phone">
         <DefaultGeneral>
           <Phone width="36" height="36" fill="whitesmoke" />
         </DefaultGeneral>
@@ -51,13 +22,13 @@ const Telephone = props => {
         <MobileLandscape>
           <Phone width="24" height="24" fill="whitesmoke" />
         </MobileLandscape>
-      </IconPhone>
-      <TelephoneNumber>
+      </div>
+      <div id="telephone-number">
         <p>T +49 221 721 64 35</p>
         <p>T +49 221 997 532 39</p>
         <p>M +49 179 731 94 55</p>
-      </TelephoneNumber>
-    </Container>
+      </div>
+    </div>
   )
 }
 
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
-// import {Link} from 'gatsby'
-// import Logo from './svg/Logo'
+import Address from '../Address/Address'
+import Telephone from '../Address/Telephone'
+import IconBouquetcat from '../icons/IconBouquetCat'
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="content">
-      
+  <footer style={{padding: '1.5rem'}} className="footer">
+    <div className="level is-mobile">
+      <div className="level-left">
+      <Address />
+      <Telephone />
+      </div>
+      <div className="level-right">
+        <IconBouquetcat height="150" />
+      </div>
     </div>
   </footer>
 )
