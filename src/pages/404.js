c697fa992ea4e93b1f82cfc556bc403273643287
akolybelnikov <a.kolybelnikov@gmail.com>
2018-09-16 00:38:06 +0200

    not-found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Responsive from 'react-responsive'
+import IconDeliveryCat from '../components/icons/IconDeliveryCat'
+
+const Mobile = props => <Responsive {...props} maxWidth={767} orientation={'portrait'}/>
+const MobileLandscape = props => (<Responsive {...props} maxWidth={823} orientation={'landscape'}/>)
+const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={1023}/>
+const Default = props => (<Responsive {...props} minWidth={1024}/>)
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0
+    }}>
+      <h2 style={{color: 'rgba(101,55,148,1)'}}>Nichts gefunden ...</h2>
+      <Default>
+        <IconDeliveryCat height="200"/>
+      </Default>
+      <Tablet>
+        <IconDeliveryCat height="185"/>
+      </Tablet>
+      <Mobile>
+        <IconDeliveryCat height="110"/>
+      </Mobile>
+      <MobileLandscape>
+        <IconDeliveryCat height="100"/>
+      </MobileLandscape>
+    </div>
   </Layout>
 )
 
